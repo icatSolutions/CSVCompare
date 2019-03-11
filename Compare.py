1ad5ab6a7419cee223dade7c10da11008bb1d6c3
George Michalopoulos <gm@icatmed.com>
2019-03-11 17:20:23 +0000

    string updates

--- Compare.py
+++ Compare.py
@@ -4,6 +4,9 @@ from os import listdir
 from os.path import isdir, join
 import pathlib
 
+#decimal precision
+precision = 5
+
 
 def compare(directory, field):
     file_list = [directory+"/"+field+'_original.csv', directory+"/"+field+'.csv']  # List of file names
@@ -13,19 +16,27 @@ def compare(directory, field):
     diffFound = 0
     print(directory)
     print(field)
-
+    
     # read file data
     with open(file_list[0]) as f:
         file1 = f.readlines()
     with open(file_list[1]) as f:
         file2 = f.readlines()
-    print("file data read")
+#   print("file data read")
 
     # remove first line from source file
     file1.pop(0);
 
+#   reference matrix
+    rowCount = (len(file1) - 1) / 2
+    columnCount = (len(file1[0].strip().split(',')[:-1]) - 1) / 2
+#   write to file
+    output.write("------------------------------------\n" + "Scanner Name: " + directory + "\n------------------------------------\n" + "Field name: " + field + ":\n" + "\nMatrix: " + str(rowCount) + "cm" + " x " + str(columnCount) + "cm\n" +  "Decimal precision: " + str(precision) + " decimals\n\n")
+
     # iterate through data
     for line in file1:
+#        row
+#        print(len(file1))
         index = file1.index(line)
         lineValues1 = line.strip().split(',')[:-1]
 
@@ -34,20 +45,23 @@ def compare(directory, field):
 
         secondFileValues = file2[index].strip().split(',')[:-1]
         for value in lineValues1:
+#            column
+#            print(len(lineValues1))
             valueIndex = lineValues1.index(value)
             secondValue = secondFileValues[valueIndex]
 
             # comparison
 #            print(index)
 #            print(valueIndex)
-            if round(float(value.strip()), 5) != round(float(secondValue.strip()), 5):
+            if round(float(value.strip()), precision) != round(float(secondValue.strip()), precision):
                 diffFound += 1
-                output.write("row:"+str(index+1)+", column:"+str(valueIndex+1)+", first value:"+ value+", second value:"+ secondValue+"\n")
+                output.write("row:"+str(index+1)+", column:"+str(valueIndex+1)+", Original value:"+ value+", Compared value:"+ secondValue+"\n")
 
-    print("csv files compared")
-    print(str(diffFound) + " difference(s) found!")
-
-
+#    print("csv files compared")
+#    print(str(diffFound) + " difference(s) found!")
+    if diffFound == 0:
+        output.write("no differences found between files")
+                 
 current_dir = pathlib.Path(__file__).parent
 dirs = [f for f in listdir(current_dir) if isdir(join(current_dir, f))]
 
